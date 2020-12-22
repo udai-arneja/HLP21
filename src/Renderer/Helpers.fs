42581455e2672da4a8f8fe56921180502b33b9d1
Tom Clarke <tomcl@users.noreply.github.com>
2020-12-22 12:30:28 +0000

    Update Helpers.fs

--- src/Renderer/Helpers.fs
+++ src/Renderer/Helpers.fs
@@ -63,11 +63,21 @@ let addTimeToStats (t:float) (st:Stats) =
 /// execution time stats indexed by name in recordExecutionStats
 let mutable executionStats = Map<string,Stats> []
 
-/// run (f arg) recording its time in executionStats under name
+/// Run (f arg) recording its time in executionStats under name.
+/// NB - this will run f multiple times if needed to estimate average speed more accurately.
+/// If an execution time of 5ms for this function is too long reduce timeLimit.
+/// The multiple time execution will not work, and will give lower than real results, if
+/// f is memoised. In that case set timeLimit to 0. for only one execution.
 let recordExecutionTimeStats (name: string) (f: 'a -> 'b) (arg: 'a) : 'b =
+    let timeLimit = 0. // time in ms to execute f for.
     let t1 = timeNowInMicroS()
-    let res = f arg
-    let t = float (timeNowInMicroS() - t1) / 1000.
+    let execTime() = float (timeNowInMicroS() - t1) / 1000.
+    let res = f arg // do f
+    let mutable iterations = 1
+    while execTime() < timeLimit do // do f multiple times if it is fast to get more accurate speed statistics
+        iterations <- iterations + 1
+        f arg |> ignore // do f again
+    let t = execTime() / float iterations
     executionStats <-
         Map.tryFind name executionStats
         |> Option.map (addTimeToStats t)
@@ -80,7 +90,7 @@ let printStats() =
     executionStats
     |> Map.toList
     |> List.iter (fun (name,st) -> 
-        printfn "%s time: min=%.2fms max=%.2fms av=%.2fms samples:%d" name st.Min st.Max st.Av (int st.Num))
+        printfn "%s time: min=%.3fms max=%.3fms av=%.3fms samples:%d" name st.Min st.Max st.Av (int st.Num))
     executionStats <- Map [] // reset stats
 
 //--------------------------------Constants----------------------------------//
